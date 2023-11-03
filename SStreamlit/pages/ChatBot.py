b70cff8d13e6ce8092df9eeca6524ad6ea4516d7
Sumanayana-Konda <konda.su@northeastern.edu>
2023-11-03 15:01:11 -0400

    Streamlit

--- SStreamlit/pages/ChatBot.py
+++ SStreamlit/pages/ChatBot.py
@@ -1,10 +1,3 @@
-import streamlit as st
-import requests
-import json
-
-# Function to handle new message submissions
-
-
 import json
 import requests
 import streamlit as st
@@ -59,35 +52,36 @@ def handle_new_message(question, file, api_key, token):
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 
-st.sidebar.subheader("Sign In")
-username = st.sidebar.text_input("Username")
-password = st.sidebar.text_input("Password", type="password")
+if 'access_token' not in st.session_state:
+    st.sidebar.subheader("Sign In")
+    username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input("Password", type="password")
 
-sign_in_button = st.sidebar.button("Sign In")
+    sign_in_button = st.sidebar.button("Sign In")
 
-if sign_in_button:
-    try:
-        response = requests.post(
-            'https://assignment-3-ec7a8fd53eb5.herokuapp.com/token',
-            data={
-                'username': username,
-                'password': password
-            }
-        )
+    if sign_in_button:
+        try:
+            response = requests.post(
+                'https://assignment-3-ec7a8fd53eb5.herokuapp.com/token',
+                data={
+                    'username': username,
+                    'password': password
+                }
+            )
 
-        if response.status_code == 200:
-            token = response.json().get('access_token', None)
-            if token:
-                st.session_state['access_token'] = token
-                st.success('You are successfully signed in!')
+            if response.status_code == 200:
+                token = response.json().get('access_token', None)
+                if token:
+                    st.session_state['access_token'] = token
+                    st.success('You are successfully signed in!')
+                else:
+                    st.error('Received status code 200 but no token was found in the response.')
             else:
-                st.error('Received status code 200 but no token was found in the response.')
-        else:
-            st.error('Login failed. Check your username and/or password.')
-    except requests.exceptions.RequestException as e:
-        st.error(f'An error occurred: {e}')
-    except Exception as e:
-        st.error(f'An unexpected error occurred: {e}')
+                st.error('Login failed. Check your username and/or password.')
+        except requests.exceptions.RequestException as e:
+            st.error(f'An error occurred: {e}')
+        except Exception as e:
+            st.error(f'An unexpected error occurred: {e}')
 
 
 
@@ -125,7 +119,7 @@ with st.form("chat_form"):
     openai_key = st.text_input('OpenAI Key', key='openai_key')
     submit_button = st.form_submit_button(label='Submit')
 
-if submit_button and question and st.session_state.get('access_token') and file and openai_key:
+if submit_button and question and 'access_token' in st.session_state and file and openai_key:
     # Call the message handler function and pass the token from session_state
     answer_data = handle_new_message(question, file, openai_key, st.session_state['access_token'])
     if answer_data and isinstance(answer_data, dict):
