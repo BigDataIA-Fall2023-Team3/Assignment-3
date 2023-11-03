f626abb510aba69ce5e75646fdeb6670b51e521f
Ruthwik Bommenahalli Gowda <ruthwik@Ruthwiks-MacBook-Air.local>
2023-11-03 15:02:38 -0400

    Airflow update

--- Airflow/dags/main.py
+++ Airflow/dags/main.py
@@ -19,9 +19,17 @@ import time
 import pinecone
 import ast
 import pkg_resources
+from airflow.models.param import Param
+import pkg_resources
 
-pinecone.init(api_key=os.getenv('PINECONE'),environment='gcp-starter')
-index = pinecone.Index('big-data')
+try:
+    pinecone.init(api_key=os.getenv('PINECONE'), environment='gcp-starter')
+    index = pinecone.Index('bigdata')
+    print("Pinecone initialization and index creation successful.")
+except Exception as e:
+    print("An error occurred:", str(e))
+
+
 s3_bucket = 'csv07'
 s3_object_key = 'extract.csv'
 openai.api_key = os.getenv('OPENAI_API')
@@ -36,17 +44,18 @@ pdf_links_list = ["https://www.sec.gov/files/form1-z.pdf",
 
 # Function to extract content from a PDF link
 def extract_pdf_content(links, output_csv_file):
-    column_names = ["Meta Data", "Text", "Embeddings"]
+    column_names = ["Filename","Metadata", "Text", "Embeddings"]
     df = pd.DataFrame(columns=column_names)
     for i in links:
+        filename = i.split('/')[-1]
         pdf_response = requests.get(i)
         pdf_content = pdf_response.content
         meta_data, pdf_text = extract_text_with_pypdf2(pdf_content)
         sentences_list = extract_sentences(pdf_text.strip())
-        # print(sentences_list[0])
+        print(sentences_list[0])
         chunk_list = create_chunk_list(sentences_list)
         embeddings_list = gen_embed(chunk_list)
-        df_temp = pd.DataFrame({'Meta Data': meta_data,'Text': chunk_list, 'Embeddings': embeddings_list})
+        df_temp = pd.DataFrame({'Filename':filename,'Metadata': meta_data,'Text': chunk_list, 'Embeddings': embeddings_list})
         df = pd.concat([df, df_temp], ignore_index=True)
     df.to_csv(output_csv_file, index=True)
     upload_csv_to_s3(output_csv_file, 'extract.csv')
@@ -149,6 +158,7 @@ def update_db(file):
     add_to_pinecone(df)
 
 
+
 def get_df(path):
     df = pd.read_csv(path)
     df['Embeddings'] = df['Embeddings'].apply(ast.literal_eval)
@@ -157,13 +167,7 @@ def get_df(path):
     
 
 def add_to_pinecone(df):
-    # Initialize the Pinecone client
-    print("in add to pinecone_______________________-")
-    print(os.getenv('PINECONE'))
-    installed_packages = pkg_resources.working_set
-    # for package in installed_packages:
-    #     print(f"{package.key}=={package.version}")
-    print(type(os.getenv('PINECONE')))
+
     df = df.rename(columns={df.columns[0]: 'Index'})
     df['Index'] = df['Index'].astype(str)
 
@@ -174,17 +178,23 @@ def add_to_pinecone(df):
         id_list = batch_df['Index'].tolist()
         embeds = batch_df['Embeddings'].tolist()
         text_list = batch_df['Text'].tolist()
-        metalist = batch_df['Meta Data'].tolist()
-        meta = [{'text': text_batch} for text_batch in zip(metalist, text_list)]
-        to_upsert = zip(id_list, embeds, meta)  
+        file_list = batch_df['Filename'].tolist()
+        metalist = batch_df['Metadata'].tolist()
+        m_list = []
+        for i in range(len(text_list)):
+            m = {'Filename': file_list[i], 'Text': text_list[i], 'Metadata': metalist[i]}
+            m_list.append(m)
+        # meta = [{'text': text_batch} for text_batch in zip(metalist, text_list)]
+        to_upsert = zip(id_list, embeds, m_list) 
         index.upsert(vectors=list(to_upsert))
+        print("after ipsert")
     #pinecone.deinit()
 
 
 
 def search_pinecone_and_return_text(query):
     xq = openai.Embedding.create(input=query, engine="text-embedding-ada-002")['data'][0]['embedding']
-    res = index.query([xq], top_k=5, include_metadata=True)
+    res = index.query([xq], top_k=1, include_metadata=True)
     
     results = []
     for match in res['matches']:
@@ -196,25 +206,10 @@ def search_pinecone_and_return_text(query):
 
 
 
-def construct_prompt(query):
-    matches = search_pinecone_and_return_text(query)
-
-  
-    flattened_matches = [item for sublist in matches for item in sublist]
-
-    prompt = """Answer the question as truthfully as possible using the context below, and if the answer is no within the context, say 'I don't know.'"""
-    prompt += "\n\n"
-    prompt += "Context: " + "\n".join(flattened_matches)
-    prompt += "\n\n"
-    prompt += "Question: " + query
-    prompt += "\n"
-    prompt += "Answer: "
-    return prompt
-
-
 
 def answer_question(results, query):
     # Build the prompt with the search results and the user's question
+    results = results[0]
     prompt = f"Context: {results}\nQuestion: {query}\nAnswer:"
 
     response = openai.ChatCompletion.create(
@@ -228,7 +223,16 @@ def answer_question(results, query):
     response_text = response['choices'][0]['message']['content']
     return response_text
 
+def qa(**kwargs):
+    q = kwargs["params"]["query"]
+    res = search_pinecone_and_return_text(q)
+    context = res[0][1]
+    a = answer_question(context,q)
+    print(a)
+    
 
+
+#############################################____________________________________#############################################
 
 dag = DAG(
     dag_id="csv_generation",
@@ -245,7 +249,7 @@ dag = DAG(
 pdf_processing_task = PythonOperator(
     task_id="pdf_extract",
     python_callable=extract_pdf_content,
-    op_args=[pdf_links_list, "output.csv"],
+    op_args=[pdf_links_list, "extract.csv"],
     dag=dag,
 )
 
@@ -253,107 +257,47 @@ pdf_processing_task = PythonOperator(
 pdf_processing_task
 
 
-# dag2 = DAG(
-#     dag_id="download",
-#     schedule_interval=None,   # Use schedule_interval instead of schedule
-#     start_date=days_ago(0),
-#     catchup=False,
-#     dagrun_timeout=timedelta(minutes=60),
-#     tags=["database"],
-#     # params=user_input,
-# )
-
-
-# pinecone = PythonOperator(
-#     task_id="database",
-#     python_callable=update_db,
-#     op_args=["output.csv"],
-#     dag=dag2,
-# )
-
-
-# pinecone_search = PythonOperator(
-#     task_id="search",
-#     python_callable=search_pinecone_and_return_text,
-#     op_args=["What is the Eligibility Requirements for Use of Form 1-A only?"],
-#     provide_context=True,
-#     dag=dag2,
-# )
-
-# prompt = PythonOperator(
-#     task_id="promt",
-#     python_callable=construct_prompt,
-#     op_args=["What is the Eligibility Requirements for Use of Form 1-A only?"],
-#     provide_context=True,
-#     dag=dag2,
-# )
-
-# question_answer=PythonOperator(
-#     task_id="qa",
-#     python_callable=answer_question,
-#     op_args=[results,"What is the Eligibility Requirements for Use of Form 1-A only?"],
-#     provide_context=True,
-#     dag=dag2,
-
-# )
-
-
-
-
-
-
-# Define your functions here
-
-dag = DAG(
-    dag_id="pdf_processing_dag",
+dag2 = DAG(
+    dag_id="download",
     schedule_interval=None,   # Use schedule_interval instead of schedule
     start_date=days_ago(0),
     catchup=False,
     dagrun_timeout=timedelta(minutes=60),
-    tags=["pdf_processing"],
+    tags=["database"],
     # params=user_input,
 )
 
-pdf_processing_task = PythonOperator(
-    task_id="pdf_extract",
-    python_callable=extract_pdf_content,
-    op_args=[pdf_links_list, "output.csv"],
-    dag=dag,
-)
 
-pinecone_task = PythonOperator(
-    task_id="update_pinecone",
+pinecone = PythonOperator(
+    task_id="database",
     python_callable=update_db,
     op_args=["output.csv"],
-    dag=dag,
+    dag=dag2,
 )
+pinecone
 
-search_pinecone_task = PythonOperator(
-    task_id="search_pinecone",
-    python_callable=search_pinecone_and_return_text,
-    op_args=["What is the Eligibility Requirements for Use of Form 1-A only?"],
-    provide_context=True,
-    dag=dag,
+user = {
+    "query" : Param(default = "Summary of form 1-a", type='string', minLength=5, maxLength=25, )
+}
+
+dag3 = DAG(
+    dag_id="querytest",
+    schedule_interval=None,  # Use schedule_interval instead of schedule
+    start_date=days_ago(0),
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=60),
+    tags=["qa"],
+    params=user,
+     # Assign the user parameter to the DAG
 )
-
-construct_prompt_task = PythonOperator(
-    task_id="construct_prompt",
-    python_callable=construct_prompt,
-    op_args=["What is the Eligibility Requirements for Use of Form 1-A only?"],
-    provide_context=True,
-    dag=dag,
+answer = PythonOperator(
+    task_id="database",
+    python_callable=qa,
+    dag=dag3,
 )
+answer
 
-question_answer_task = PythonOperator(
-    task_id="qa",
-    python_callable=answer_question,
-    op_args=[search_pinecone_task, "What is the Eligibility Requirements for Use of Form 1-A only?"],
-    provide_context=True,
-    dag=dag,
-)
+user_link = {
+    "link" : Param(type='string', minLength=5, maxLength=100, )
+}
 
-# Set up task dependencies
-pdf_processing_task >> pinecone_task
-pinecone_task >> search_pinecone_task
-search_pinecone_task >> construct_prompt_task
-construct_prompt_task >> question_answer_task
